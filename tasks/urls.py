64818ee4381a4bfb1bbd72d1e9ec2c88bbd14f94
Priyanka Pradhan <priyankapradhan212@gmail.com>
2025-03-26 02:41:12 +1300

    Added task list view with status tracking and updated task creation UI

--- tasks/urls.py
+++ tasks/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('create/', views.create_task, name='create_task'),
     path('logout/', views.user_logout, name='logout'),  # Logout page
+    path('tasks/', views.task_list, name='task_list'),  # View all tasks
 ]
