3e619c73954e0cce2ce93cef13dbf175c58bea29
Priyanka Pradhan <priyankapradhan212@gmail.com>
2025-04-01 22:55:07 +1300

    Install PostgreSQL dependencies during build

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Install Python (if it's not installed already)
+# Update apt package list and install dependencies
 apt-get update
-apt-get install -y python3 python3-pip
 
-# Install dependencies
+# Install Python 3, pip, and PostgreSQL development libraries
+apt-get install -y python3 python3-pip libpq-dev
+
+# Install Python dependencies
 pip3 install -r requirements.txt
 
 # Apply database migrations
