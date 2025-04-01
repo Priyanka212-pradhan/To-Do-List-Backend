1673b7bac842b7d4d87d2cda363373c7a9e89083
Priyanka Pradhan <priyankapradhan212@gmail.com>
2025-04-01 22:53:01 +1300

    Fix build script to install Python and dependencies

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput
+
+# Install Python (if it's not installed already)
+apt-get update
+apt-get install -y python3 python3-pip
+
+# Install dependencies
+pip3 install -r requirements.txt
+
+# Apply database migrations
+python3 manage.py migrate
+
+# Collect static files
+python3 manage.py collectstatic --noinput
